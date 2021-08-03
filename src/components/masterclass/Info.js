8a51b2cecb767bad723703b112db3633a32604c9
Mikhail Golovai <mikhail.golovai@gmail.com>
2021-08-03 13:50:07 +0300

    Revert "Revert "Bugfix/images scale""

--- src/components/masterclass/Info.js
+++ src/components/masterclass/Info.js
@@ -56,7 +56,7 @@ export default function Info ({mcs, name}) {
         >
             <Modal.Header>{first.name}</Modal.Header>
             <Modal.Content image scrolling>
-                <Image size='medium' src={first.photo ? `${first.photo}` : eye} spaced="right" />
+                <Image size='huge' src={first.photo ? `${first.photo}` : eye} spaced="right" wrapped/>
                 <Tab  
                   menu={{ fluid: true, vertical: true, tabular: 'right' }}
                   panes={panes}
@@ -75,13 +75,13 @@ export default function Info ({mcs, name}) {
                     icon='angle left'
                     onClick={() => setOpen(false)}
                 />
-                <Button 
+                {/*                <Button
                     content="Записаться" 
                     labelPosition='right'
                     icon='checkmark'
                     positive
                     onClick={() => dispatch(registerMasterclassRequest({_id: mcs[activeTab]._id}))}
-                />
+                />*/}
             </Modal.Actions>
         </Modal>
     
